47a6cd5f197903bc91eb43a32e789cf57307b196
史斌 <assasin0308@sina.com>
2020-06-09 23:12:21 +0800

    tornado introduce

---
+++ base/test.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+import tornado.web
+import tornado.ioloop
+
+class IndexHandler(tornado.web.RequestHandler):
+    """主页处理类"""
+    def  get(self):
+        """GET请求方式"""
+        self.write("hello world")
+
+    def post(self):
+        self.write('post methods')
+
+
+if __name__ == '__main__':
+    app = tornado.web.Application([(
+        r"/",IndexHandler
+    )])
+    app.listen(port=8000)
+    tornado.ioloop.IOLoop.current().start()
